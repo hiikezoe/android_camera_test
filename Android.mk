191dc105190bd7311ec342333f45c54e3b49d005
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-02-27 20:42:23 +0900

    Added a space between LOCAL_CFLAGS and ':'.

--- Android.mk
+++ Android.mk
@@ -29,7 +29,7 @@ DLOPEN_LIBMMCAMERA:=1
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS:=                                \
+LOCAL_CFLAGS :=                               \
   -DDLOPEN_LIBMMCAMERA=$(DLOPEN_LIBMMCAMERA)  \
   -DMSM_MAX_CAMERA_SENSORS=1                  \
   -DNUM_PREVIEW_BUFFERS=4
